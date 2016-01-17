4b7e49171d07df438110a9794bac51f0b959b61f
Chris Samuel <ksamuel.chris@icloud.com>
2016-01-16 23:55:15 -0500

    fixed type in login.js file that cause the Auth object to jump to the yahoo search

--- public/controllers/login.js
+++ public/controllers/login.js
@@ -2,7 +2,6 @@ angular.module('MyApp')
     .controller('LoginCtrl', ['$scope', 'Auth', function($scope, Auth) {
       $scope.login = function() {
       Auth.login({ email: $scope.email, password: $scope.password });
-    };
     $scope.facebookLogin = function() {
       Auth.facebookLogin();
     };
@@ -11,3 +10,12 @@ angular.module('MyApp')
     };
     $scope.pageClass = 'fadeZoom';
   });
+
+
+
+  angular.module('MyApp')
+    .controller('LoginCtrl', ['$scope', 'Auth', function($scope, Auth) {
+      $scope.login = function() {
+        Auth.login({email: $scope.email, password: $scope.password });
+      };
+    }]);
